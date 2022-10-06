f0b24cf12b0fb6777937cee8712a5500567908bc
mahesh862000 <75084031+mahesh862000@users.noreply.github.com>
2022-10-07 01:26:31 +0530

    Update cfsender.java

--- Sem 3/CFL/Practicals/cfsender.java
+++ Sem 3/CFL/Practicals/cfsender.java
@@ -3,21 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package cf1;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.*;
+
 /**
  *
- * @author Merin
+ * @author admin
  */
-public class cfsender {
+public class CF1 {
+   
     public static void main(String[] args) throws IOException {
         int i = 0, k = 0;
         String s = "";
         String ct = "";
         String key = "";
-        Socket sc = new Socket("localhost", 3306);
+        Socket sc = new Socket("localhost", 6017);
         Random r = new Random();
         System.out.println("Enter the string: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,5 +43,5 @@ public class cfsender {
             bw.close();
         }
     }
-        
+   
 }
